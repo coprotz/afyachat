721844d8f80c5b2a817ff7467e792433ecde921d
barruTech <tipwindi26@gmail.com>
2022-09-09 00:03:08 +0300

    last

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,10 +8,10 @@ const About = () => {
         <Navbar/>
         <div className="about_inner">
            <h1 className='about_title'>About</h1>
-           <h3 className='about_desc'>We are dedicating in providing Proffessioal Health Care for all..!</h3>
-           <p>Through our Platform that enabling Tanzanians to acquire medical and healthcare consultations 
+           <h3 className='about_desc'>We are dedicated in providing Proffessional Health Care for all..!</h3>
+           <p>Through our Platform that enables Tanzanians to acquire medical and healthcare consultations 
             from specialized and competent Doctors at convenient way and lowest rates</p>
-            <p>One stop solutions for medical and clinical for all your healthcare problems</p>
+            <p>One stop solution for medical and clinical advices for all your healthcare problems!</p>
         </div>
       
     </div>
